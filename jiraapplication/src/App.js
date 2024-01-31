4ece98a66a2f9c300634bd5ae343a127771a75c3
kubraacelik <kkbra.celik92@gmail.com>
2024-01-31 21:23:02 +0300

    provided the user with an interface to add, edit and delete tasks

---
+++ jiraapplication/src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (title, taskDesc) => {
+    //her yeni eleman oluştuğunda çalışsın
+    const createdTasks = [
+      ...tasks,
+      {
+        id: Math.round(Math.random() * 999999),
+        title: title,
+        taskDesc: taskDesc,
+      },
+    ];
+    setTasks(createdTasks);
+  };
+
+  //sil butonu
+  const deleteTaskById = (id) => {
+    const afterDeletingTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(afterDeletingTasks);
+  };
+
+  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+    const UpdatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { id: id, title: updatedTitle, taskDesc: updatedTaskDesc };
+      }
+      return task;
+    });
+    setTasks(UpdatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <TaskCreate onCreate={createTask} />
+      <h1>Görevler</h1>
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onUpdate={editTaskById}
+      />
+    </div>
+  );
+}
+export default App;
