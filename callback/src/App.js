942986a3c728ae44442b4e7e579d3a17337d12e5
kubraacelik <kkbra.celik92@gmail.com>
2024-02-07 17:05:25 +0300

    learned useCallBack hook

--- callback/src/App.js
+++ callback/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  //number değiştiğinde getItems'ı tetikle
+  //number değiştiğinde getItems'ı tetikle dedik
   const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
   }, [number]);
