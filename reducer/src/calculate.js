d9af978f2e9e727762c5c000d13acc4ba114d6e9
kubraacelik <kkbra.celik92@gmail.com>
2024-02-09 13:56:10 +0300

    added NumberContext

--- reducer/src/calculate.js
+++ reducer/src/calculate.js
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { NumberContext } from "./App";
 
 function Calculate() {
+  //app.js'de açtığım NumberContext'i numberContext adıyla burada kullandım
   const numberContext = useContext(NumberContext);
+
   return (
     <div>
       <div>Sayı : {numberContext.count}</div>
