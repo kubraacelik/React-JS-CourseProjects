5b72a7632089192311115eb5d05394a87cc236c9
kubraacelik <kkbra.celik92@gmail.com>
2024-02-09 20:54:12 +0300

    used custom hooks

---
+++ customhook/src/TitleOne.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+import useTitle from "./useTitle";
+
+function TitleOne() {
+  const [num, setNum] = useState(0);
+
+  return <div></div>;
+}
+
+export default TitleOne;
