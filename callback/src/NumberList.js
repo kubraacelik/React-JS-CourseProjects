918928adec900f47b56ad66bd619c2d2fb51011d
kubraacelik <kkbra.celik92@gmail.com>
2024-02-09 14:14:22 +0300

    we suppress each element in a div

--- callback/src/NumberList.js
+++ callback/src/NumberList.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 
 function NumberList({ getItems }) {
   useEffect(() => {
+    //setItems'ı kullanarak items'ın değerlerinin atamasını yapıyoruz
     setItems(getItems());
     console.log("updating Items");
   }, [getItems]);
   const [items, setItems] = useState([]);
+  //her bir elemana item dedik ve div içinde bastırdık
   return items.map((item) => <div key={item}>{item}</div>);
 }
 
