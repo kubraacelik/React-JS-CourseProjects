0fe70077142fd8a8bbf6bdafdd198f3a51cf91da
kubraacelik <kkbra.celik92@gmail.com>
2024-02-09 20:51:28 +0300

    learned custom hooks

---
+++ customhook/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./Counter";
+import PersonalInfo from "./PersonalInfo";
+import TitleOne from "./TitleOne";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TitleOne /> */}
+      {/* <Counter /> */}
+      <PersonalInfo />
+    </div>
+  );
+}
+
+export default App;
