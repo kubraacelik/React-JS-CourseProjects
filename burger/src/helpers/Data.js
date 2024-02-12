100fa7e6177aaab594c2608454fedf36c1533bfe
kubraacelik <kkbra.celik92@gmail.com>
2024-02-12 20:06:32 +0300

    added data

---
+++ burger/src/helpers/Data.js
@@ -0,0 +1,98 @@
+import React from "react";
+import Burger1 from "../assets/burger1.jpg";
+import Burger2 from "../assets/burger2.jpg";
+import Burger3 from "../assets/burger3.jpg";
+import Burger4 from "../assets/burger4.jpg";
+import Burger5 from "../assets/burger5.jpg";
+import Burger6 from "../assets/burger6.jpg";
+import Burger7 from "../assets/burger7.jpg";
+import Burger8 from "../assets/burger8.jpg";
+import Burger9 from "../assets/burger9.jpg";
+import Burger10 from "../assets/burger10.jpg";
+import Burger11 from "../assets/burger11.jpg";
+import Burger12 from "../assets/burger12.jpg";
+
+export const Data = [
+  {
+    name: "Acılı Buluşma Burgeri",
+    image: Burger1,
+    content: "Mangolu acı soslu hamburger ekmeği ile lezzet patırtısı yaşayın!",
+    price: 210,
+  },
+  {
+    name: "Meksika Rüya Burgeri",
+    image: Burger2,
+    content:
+      "Jalapeno ve cheddar peyniriyle Meksika'nın lezzetlerine taşıyan burger   .",
+    price: 270,
+  },
+  {
+    name: "Ziyafet Burgeri",
+    image: Burger3,
+    content: "Özel sos ile deniz kenarındaymış hissi veren bir lezzet şöleni.",
+    price: 250,
+  },
+  {
+    name: "Mangal Keyfi Burgeri",
+    image: Burger4,
+    content:
+      "Barbekü sosu, ve karamelize soğan ile zenginleştirilmiş, enfes bir mangal lezzeti.",
+    price: 270,
+  },
+  {
+    name: "Akdeniz Burgeri",
+    image: Burger5,
+    content:
+      "Taze domates ve etle dolu, Akdeniz esintilerini hissettiren bir lezzet yolculuğu.",
+    price: 280,
+  },
+  {
+    name: "Güzellik Burgeri",
+    image: Burger6,
+    content:
+      "Quinoa köfte ve özel sos ile sağlıklı ve nefis bir burger deneyimi.",
+    price: 260,
+  },
+  {
+    name: "Tatlı Kaos Burgeri",
+    image: Burger7,
+    content:
+      " Karamel sos ile dolu, lezzet krizine davetiye çıkaran bir burger.",
+    price: 275,
+  },
+  {
+    name: "Asya Çılgın Burgeri",
+    image: Burger8,
+    content:
+      "Soya soslu tavuk, sebzeler ve teriyaki sosu ile Asya mutfağının lezzetlerini keşfedin.",
+    price: 290,
+  },
+  {
+    name: "Mistik Orman Burgeri",
+    image: Burger9,
+    content:
+      "Trüf yağı ile zenginleştirilmiş, ormanın büyülü dokunuşunu taşıyan bir burger.",
+    price: 255,
+  },
+  {
+    name: "Amerikan Burgeri",
+    image: Burger10,
+    content:
+      "Dana köfte ve turşu ile Amerikan lezzetini saf haliyle sunan burger.",
+    price: 295,
+  },
+  {
+    name: "Rustik Ziyafet Burgeri",
+    image: Burger11,
+    content:
+      "Füme peynir ve kavrulmuş soğan  ile rustik bir lezzet şölenine davet eden bir burger.",
+    price: 285,
+  },
+  {
+    name: "Feast Fiesta Burgeri",
+    image: Burger12,
+    content:
+      "Renkli biberler ve salsa sos ile dolu, parti başlasın dedirten bir festival lezzeti!",
+    price: 300,
+  },
+];
