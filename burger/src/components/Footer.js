c4328ef85713bdaadb05b7e19054e22d9f21821c
kubraacelik <kkbra.celik92@gmail.com>
2024-02-12 14:55:00 +0300

    added component

---
+++ burger/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <FacebookIcon />
+        <InstagramIcon />
+        <XIcon />
+      </div>
+      <p>Tüm Hakları Saklıdır</p>
+    </div>
+  );
+};
+
+export default Footer;
