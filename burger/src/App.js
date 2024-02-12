71f82a88a95e78d975ecb78704a37936911c5bc9
kubraacelik <kkbra.celik92@gmail.com>
2024-02-12 14:55:24 +0300

    added navbar and footer

--- burger/src/App.js
+++ burger/src/App.js
@@ -4,16 +4,20 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/about" exact element={<About />} />
         <Route path="/contact" exact element={<Contact />} />
         <Route path="/menu" exact element={<Menu />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
