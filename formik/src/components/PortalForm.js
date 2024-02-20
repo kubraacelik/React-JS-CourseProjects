f35fe632bea563ec5a3d2902c2fef4d6d0ea92c0
kubraacelik <kkbra.celik92@gmail.com>
2024-02-20 19:34:46 +0300

    Input, select and checkbox customizations added into PortalFrom component

--- formik/src/components/PortalForm.js
+++ formik/src/components/PortalForm.js
@@ -1,7 +1,67 @@
 import React from "react";
+import { Form, Formik } from "formik";
+import CustomInput from "./CustomInput";
+import { advancedSchema } from "../schemas";
+import CustomSelect from "./CustomSelect";
+import CustomCheckbox from "./CustomCheckbox";
+import { Link } from "react-router-dom";
+
+//submit ile ilgili ayarlamalarımız burada olacak.
+//values = input'a girdiğim değerler, actions = form resetleme gibi aksiyonlar yer alıyor.
+const onSubmit = async (values, actions) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+  actions.resetForm();
+};
 
 function PortalForm() {
-  return <div>PortalForm</div>;
+  return (
+    <>
+      <Formik
+        initialValues={{
+          username: "",
+          university: "",
+          isAccepted: false,
+        }}
+        validationSchema={advancedSchema}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            {/* Kullanıcı Adı kısmı */}
+            <CustomInput
+              label="Kullanıcı Adı"
+              name="username"
+              type="text"
+              placeholder="Kullanıcı Adınızı Giriniz"
+              autoComplete="off"
+            />
+            {/* Üniversite Seçme Kısmı */}
+            <CustomSelect
+              label="Okulunuz"
+              name="university"
+              placeholder="Lütfen Üniversitenizi Seçiniz"
+            >
+              <option value="">Lütfen Üniversitenizi Seçiniz</option>
+              <option value="bogazici">Boğaziçi Üniversitesi</option>
+              <option value="gsu">Galatasaray Üniversitesi</option>
+              <option value="odtü">Orta Doğu Teknik Üniversitesi</option>
+              <option value="itü">İstanbul Teknik Üniversitesi</option>
+            </CustomSelect>
+            {/* Kullanım Koşulu Onaylama Kısmı */}
+            <CustomCheckbox type="checkbox" name="isAccepted" />
+            <button disabled={isSubmitting} type="submit">
+              Kaydet
+            </button>
+            <Link className="formLink" to="/">
+              Ana Forma Git
+            </Link>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 }
 
 export default PortalForm;
