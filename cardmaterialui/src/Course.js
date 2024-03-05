a87fba6c191fe9178c845c0192ec1eb326462e38
kubraacelik <kkbra.celik92@gmail.com>
2024-03-05 19:51:50 +0300

    added Card UI

---
+++ cardmaterialui/src/Course.js
@@ -0,0 +1,22 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+function Course({ image, title, description }) {
+  return (
+    <Card>
+      <CardMedia sx={{objectFit: 'cover', width: '100%', height:330}} image={image} title={title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Course;
