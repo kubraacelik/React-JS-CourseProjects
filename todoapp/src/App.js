869c45b8a5c2aee267dfdf831aeae9942490f835
kubraacelik <kkbra.celik92@gmail.com>
2024-03-07 17:34:28 +0300

    added Task and Footer components

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,12 +1,49 @@
 import "./App.css";
 import CreateTask from "./CreateTask";
+import Footer from "./Footer";
 import Header from "./Header";
+import Task from "./Task";
+import { useState } from "react";
 
 function App() {
+  // listelemek için
+  const [tasks, setTasks] = useState([]);
+
+  // eklemek için
+  const addTask = (newTask) => {
+    setTasks((prevTask) => {
+      return [...prevTask, newTask];
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((prevTask) => {
+      return prevTask.filter((taskItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div className="App">
       <Header />
-      <CreateTask />
+      <CreateTask onAdd={addTask} />
+      <div className="container">
+        <div className="row my-5">
+          {tasks.map((taskItem, index) => {
+            return (
+              <Task
+                key={index}
+                id={index}
+                title={taskItem.title}
+                content={taskItem.content}
+                onDelete={deleteTask}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
