6e5ae3abe5e462385fc3c8eaf819374cd34f101d
kubraacelik <kkbra.celik92@gmail.com>
2024-03-07 16:00:07 +0300

    updated app.js

---
+++ todoapp/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CreateTask from "./CreateTask";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CreateTask />
+    </div>
+  );
+}
+
+export default App;
