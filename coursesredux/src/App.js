31fdcfb3136a3b3e84b6f33b5921218fa663a1dd
kubraacelik <kkbra.celik92@gmail.com>
2024-03-08 21:12:10 +0300

    first commit

---
+++ coursesredux/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CourseList from "./components/CourseList";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <CourseList />
+    </div>
+  );
+}
+
+export default App;
