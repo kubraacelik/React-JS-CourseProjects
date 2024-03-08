69ae485f025ef6ed54c7aa282155fd4a547045e7
kubraacelik <kkbra.celik92@gmail.com>
2024-03-08 21:13:59 +0300

    added CourseItems object

---
+++ coursesredux/src/courseItems.js
@@ -0,0 +1,32 @@
+const courseItems = [
+  {
+    id: "1",
+    title: "C Programlama Eğitimi",
+    price: "89",
+    img: "https://www.canboz.com/Assets/img/canboz/c.png",
+    quantity: 1,
+  },
+  {
+    id: "2",
+    title: "C# Programlama Eğitimi",
+    price: "105",
+    img: "https://www.canboz.com/Assets/img/canboz/csharp.png",
+    quantity: 1,
+  },
+  {
+    id: "3",
+    title: "Typescript Eğitimi",
+    price: "102",
+    img: "https://www.canboz.com/Assets/img/canboz/typescript.jpg",
+    quantity: 1,
+  },
+  {
+    id: "1",
+    title: "React Eğitimi",
+    price: "114",
+    img: "https://www.canboz.com/Assets/img/canboz/react.png",
+    quantity: 1,
+  },
+];
+
+export default courseItems;
