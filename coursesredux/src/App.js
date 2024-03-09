441df9495a54b6fd20fe19487d3a5823bbb38294
kubraacelik <kkbra.celik92@gmail.com>
2024-03-09 15:49:39 +0300

    added useEffect

--- coursesredux/src/App.js
+++ coursesredux/src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import CourseList from "./components/CourseList";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { calculateTotal } from "./control/cardSlice";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const { cardItems } = useSelector((store) => store.card);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cardItems]);
+
   return (
     <div className="App">
       <Navbar />
