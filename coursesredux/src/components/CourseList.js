dd0752f17f03254852f6d832347059511ade0c3a
kubraacelik <kkbra.celik92@gmail.com>
2024-03-09 15:48:52 +0300

    added CourseList component

--- coursesredux/src/components/CourseList.js
+++ coursesredux/src/components/CourseList.js
@@ -1,26 +1,33 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CourseItem from "./CourseItem";
+import { clearCard } from "../control/cardSlice";
+
+//? store'daki initialState'e ulaşıp kursları listelemem lazım
 
 function CourseList() {
+  const dispatch = useDispatch();
+
+  // useSelector yardımıyla store'daki bilgileri aldım
   const { cardItems, quantity, total } = useSelector((store) => store.card);
+
   return (
     <>
       {quantity < 1 ? (
-        <section>
+        <section className="card">
           <header>
             <h2>SEPETİM</h2>
             <h4>BOMBOŞ</h4>
           </header>
         </section>
       ) : (
-        <section>
+        <section className="card">
           <header>
             <h2>SEPETİM</h2>
           </header>
           <div>
             {cardItems.map((item) => {
-              return <CourseItem {...item} />;
+              return <CourseItem key={item.id} {...item} />;
             })}
           </div>
           <footer>
@@ -30,7 +37,12 @@ function CourseList() {
                 Toplam Tutar : <span>{total} TL </span>
               </h4>
             </div>
-            <button>TEMİZLE</button>
+            <button
+              className="cardClearButton"
+              onClick={() => dispatch(clearCard())}
+            >
+              TEMİZLE
+            </button>
           </footer>
         </section>
       )}
