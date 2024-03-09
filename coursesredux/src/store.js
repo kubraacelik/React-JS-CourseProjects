668cabd7c7ac8ef8c1555f68ec4e5b47d1af82ae
kubraacelik <kkbra.celik92@gmail.com>
2024-03-09 15:47:43 +0300

    defined reducers

--- coursesredux/src/store.js
+++ coursesredux/src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cardReducer from "./control/cardSlice";
 
 //?Tüm uygulamalarda geçerli state yönetimini yapabileceğimiz store oluşturduk
+
 export const store = configureStore({
   // Tüm işlemleri reducer'de yapacağız
   reducer: {
