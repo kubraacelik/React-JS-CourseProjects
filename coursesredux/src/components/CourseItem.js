355c64a2d858f3ab1fc6d9878fc91e2185fc159d
kubraacelik <kkbra.celik92@gmail.com>
2024-03-09 15:49:26 +0300

    added CourseItem component

--- coursesredux/src/components/CourseItem.js
+++ coursesredux/src/components/CourseItem.js
@@ -1,25 +1,47 @@
 import React from "react";
 import { FaChevronUp } from "react-icons/fa";
 import { FaChevronDown } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { removeItem } from "../control/cardSlice";
+import { increase } from "../control/cardSlice";
+import { decrease } from "../control/cardSlice";
 
 function CourseItem({ id, title, price, img, quantity }) {
+  const dispatch = useDispatch();
+
   return (
-    <div>
+    <div className="cardItem">
       <img src={img} alt="" />
-      <div>
+      <div className="cardInfo">
         <h4>{title}</h4>
         <h4>{price} TL</h4>
         <div>
-          <button>
+          <button 
+          className="cardQuantityButton" 
+          onClick={() => {
+            dispatch(increase(id))
+            }}
+            >
             <FaChevronUp />
           </button>
-          <p>{quantity}</p>
-          <button>
+          <p className="cardQuantity">{quantity}</p>
+          <button className="cardQuantityButton" 
+          onClick={() => {
+            dispatch(decrease(id))
+            }}
+            >
             <FaChevronDown />
           </button>
         </div>
+        <button
+          className="cardDeleteButton"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          SİL
+        </button>
       </div>
-      <button>SİL</button>
     </div>
   );
 }
