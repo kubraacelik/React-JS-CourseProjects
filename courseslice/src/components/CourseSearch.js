2032a80a2220dcc8ffe890e422fb953767d2d4d5
kubraacelik <kkbra.celik92@gmail.com>
2024-03-10 19:30:55 +0300

    added useDispatch and useSelector

--- courseslice/src/components/CourseSearch.js
+++ courseslice/src/components/CourseSearch.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store/slices/courseSlice";
 
 function CourseSearch() {
-  return <div>CourseSearch</div>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => {
+    return state.course.searchTerm;
+  });
+
+  return (
+    <div className="listHeader">
+      <h3 className="title is-3">Kurslarım</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Ara</label>
+        <input
+          className="input"
+          onChange={(event) => {
+            dispatch(changeSearchTerm(event.target.value));
+          }}
+          value={searchTerm}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CourseSearch;
