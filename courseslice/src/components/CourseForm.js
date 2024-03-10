17804198907fa686da1ff02368ac6de5b75c994d
kubraacelik <kkbra.celik92@gmail.com>
2024-03-10 17:35:55 +0300

    created HTML for form and added dispatch for name, description and cost inputs

--- courseslice/src/components/CourseForm.js
+++ courseslice/src/components/CourseForm.js
@@ -1,7 +1,74 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  changeName,
+  changeDescription,
+  changeCost,
+} from "../store/slices/formSlice";
+import { addCourse } from "../store/slices/courseSlice";
 
 function CourseForm() {
-  return <div>CourseForm</div>;
+  const dispatch = useDispatch();
+
+  //bu bilgilere ulaşmak için useSelector kullandık
+  const { name, description, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      description: state.form.description,
+      cost: state.form.cost,
+    };
+  });
+  console.log(name, description, cost);
+
+  //form submit olunca
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCourse({ name, description, cost }));
+  };
+
+  return (
+    <div className="courseForm panel">
+      <h4 className="subtitle is-3">Kurs Ekle</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Ad</label>
+            <input
+              className="input is-expanded"
+              onChange={(event) => {
+                dispatch(changeName(event.target.value));
+              }}
+              value={name}
+            />
+          </div>
+          <div className="field">
+            <label className="label">Açıklama</label>
+            <textarea
+              className="input is-expanded"
+              onChange={(event) => {
+                dispatch(changeDescription(event.target.value));
+              }}
+              value={description}
+            />
+          </div>
+          <div className="field">
+            <label className="label">Fiyat</label>
+            <input
+              className="input is-expanded"
+              type="number"
+              onChange={(event) => {
+                dispatch(changeCost(parseInt(event.target.value)));
+              }}
+              value={cost}
+            />
+          </div>
+          <div className="field">
+            <button className="button is-primary">Kaydet</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CourseForm;
