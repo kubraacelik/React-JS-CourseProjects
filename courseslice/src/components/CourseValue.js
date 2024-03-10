5350a5aa848fde9cf60941f7a8fec69bbd2d846c
kubraacelik <kkbra.celik92@gmail.com>
2024-03-10 19:31:20 +0300

    added filtered for cost

--- courseslice/src/components/CourseValue.js
+++ courseslice/src/components/CourseValue.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function CourseValue() {
-  return <div>CourseValue</div>;
+  const totalCost = useSelector(({ form, course: { data, searchTerm } }) => {
+    return data.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ).reduce((acc,item)=>acc+item.cost,0)
+
+    // let cost = 0;
+
+    // for (let item of filteredCourses) {
+    //   cost += item.cost;
+    // }
+
+    // return cost;
+
+  });
+
+  return <div className="coursePrice">Toplam Tutar : {totalCost}</div>;
 }
 
 export default CourseValue;
