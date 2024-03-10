ebe742e74ba8c73f22b77ed4e175c0a9aa13ce57
kubraacelik <kkbra.celik92@gmail.com>
2024-03-10 17:31:19 +0300

    import store and Provider

--- courseslice/src/index.js
+++ courseslice/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bulma/css/bulma.css";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
