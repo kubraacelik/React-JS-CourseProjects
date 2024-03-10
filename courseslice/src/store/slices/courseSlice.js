e071f8070f10c15205be7c7ec4be05246fe066a3
kubraacelik <kkbra.celik92@gmail.com>
2024-03-10 19:31:57 +0300

    added changeSearchTerm and removeCourse

--- courseslice/src/store/slices/courseSlice.js
+++ courseslice/src/store/slices/courseSlice.js
@@ -20,8 +20,18 @@ const courseSlice = createSlice({
         id: nanoid(),
       });
     },
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    removeCourse(state, action) {
+      const updatedCourses = state.data.filter((item) => {
+        return item.id !== action.payload;
+      });
+      state.data = updatedCourses;
+    },
   },
 });
 
-export const { addCourse } = courseSlice.actions;
+export const { addCourse, changeSearchTerm, removeCourse } =
+  courseSlice.actions;
 export const courseReducer = courseSlice.reducer;
