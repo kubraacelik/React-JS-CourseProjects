8e41a778334eceeb4a532469138d042b87fb7a3a
kubraacelik <kkbra.celik92@gmail.com>
2024-03-10 17:36:31 +0300

    updated store for form and course

---
+++ courseslice/src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { formReducer } from "./slices/formSlice";
+import { courseReducer } from "./slices/courseSlice";
+
+//form'u formReducer yönetiyor, course'yi courseReducer yönetiyor
+export const store = configureStore({
+  reducer: {
+    form: formReducer,
+    course: courseReducer,
+  },
+});
